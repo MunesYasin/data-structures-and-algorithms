f592a6b94febef89dbe821d150e645029601aff1
MunesYasin <m2nesyasin@gmail.com>
2021-11-22 19:58:01 +0200

    add

---
+++ 401-Code-Challenge/tree-breadth-first/binaryTree.js
@@ -0,0 +1,109 @@
+'use strict'
+
+class BinaryTree {
+    constructor(root = null){
+        this.root = root
+    }
+
+    PreOrder(){
+        let array =[]
+        const _walk=(node)=>{
+            array.push(node.value)
+            if(node.left){
+                _walk(node.left)
+            }
+            if (node.right){
+                _walk(node.right)
+            }
+        }
+        _walk(this.root)
+        return array
+    }
+
+    PostOrder(){
+        let array =[]
+        const _walk=(node)=>{
+
+           
+            if(node.left){
+                _walk(node.left)
+            }
+            if (node.right){
+                _walk(node.right)
+            } 
+            array.push(node.value)
+        }
+        _walk(this.root)
+        return array
+    }
+
+    InOrder(){
+        let array =[]
+        const _walk=(node)=>{
+           
+            if(node.left){
+                _walk(node.left)
+            }
+              array.push(node.value)
+
+            if (node.right){
+                _walk(node.right)
+            }
+        }
+        _walk(this.root)
+        return array
+    }
+
+     Max(){
+        /* let array = this.PreOrder()
+        let max = array.reduce((acc,current)=>{
+            if(acc>current)
+            {return acc}else{
+                return current
+            }
+        },0)
+        return max*/
+
+
+        let max = null;
+    let currentNode = this.root;
+    if (!currentNode) { 
+      return null;
+    } else {
+      let traverseTree = currentNode => {
+        if (currentNode.value > max) { max = currentNode.value; }
+        if (currentNode.left) { traverseTree(currentNode.left); }
+        if (currentNode.right) { traverseTree(currentNode.right); }
+      };
+      traverseTree(currentNode);
+      return max;
+    }
+        }
+
+        BreadthFirst(tree){
+         let array = []
+         let tempQueue = [];
+
+         let temp = tree.root
+         tempQueue.push(temp)
+    while(tempQueue.length>0){
+      let currentNode = tempQueue[0]
+
+      if (currentNode.left !== null) {
+          tempQueue.push(currentNode.left)
+         
+      }
+
+      if (currentNode.right !== null) {
+          tempQueue.push(currentNode.right)
+      }
+
+      array.push(tempQueue.shift().value)
+  }
+  return array;
+  }
+         
+
+
+}
+module.exports=BinaryTree
