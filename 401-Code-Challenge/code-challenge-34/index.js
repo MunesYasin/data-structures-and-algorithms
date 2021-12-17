eb10ebb0471371f50768268a95d4256b5e5ec719
MunesYasin <m2nesyasin@gmail.com>
2021-12-17 19:19:48 +0200

    add

---
+++ 401-Code-Challenge/code-challenge-34/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const hashMap = require('./HashTable/hash')
+
+const myHash = new hashMap(500)
+
+
+// myHash.set('mishmish', 'getHash');
+// myHash.set('boogie', 'boogie');
+// myHash.set('🍕', '🍕');
+// myHash.set('nimnim', 'nimnim');
+// myHash.set('namlah', 'namlah');
+// myHash.set('🦝', '🦝');
+
+// myHash.map.forEach(data => {
+//      console.log(data);
+//   });
+
+ console.log (myHash.repeatedWord("No. Try not. Do or do not. There is no try."))
+//  console.log (myHash.contain("mishmish"))
+
+// console.log(myHash)
